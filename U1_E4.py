9f18dffef0c4a379363a0fd6bdae074ed1234a99
gxgarciat <info@gxgarciat.dev>
2021-06-18 16:26:41 +0100

    Unit 1 - Finger exercises01

---
+++ U1_E4.py
@@ -0,0 +1,19 @@
+def main():
+
+    """
+    Convert the following code into code that uses a for loop.
+
+    prints 2
+    prints 4
+    prints 6
+    prints 8
+    prints 10
+    prints Goodbye!
+    """
+    for i in range(2, 11, 2):
+        print(i)
+
+    print('Goodbye!')
+
+if __name__ == "__main__":
+    main()
