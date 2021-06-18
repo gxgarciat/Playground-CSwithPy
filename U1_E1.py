a23a6dee5b9b7f0fef0ce54a8f2d4c899eaac597
gxgarciat <info@gxgarciat.dev>
2021-06-18 16:05:33 +0100

    Unit 1 - Finger exercises01

---
+++ U1_E1.py
@@ -0,0 +1,22 @@
+def main():
+
+    """
+    Convert the following into code that uses a while loop.
+
+    prints 2
+    prints 4
+    prints 6
+    prints 8
+    prints 10
+    prints Goodbye!
+
+    :return:
+    """
+    i = 0
+    while i < 10:
+        i = i + 2
+        print(i)
+    print('Goodbye!')
+
+if __name__ == "__main__":
+    main()
